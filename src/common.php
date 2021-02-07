c0fd19da0eef01d57ba2a20a5c65d6db9406262b
Alvis Zhang <alvis@1979s.com>
2021-02-07 19:02:10 +0800

    add Statics

--- src/common.php
+++ src/common.php
@@ -20,6 +20,7 @@ define('FOOZ79_ROOT_DIR', FOOZ79_PROJECT_ROOT . DIRECTORY_SEPARATOR);
 define('FOOZ79_RUNTIME_DIR', FOOZ79_ROOT_DIR . 'runtime' . DIRECTORY_SEPARATOR);
 define('FOOZ79_CONFIG_DIR', FOOZ79_ROOT_DIR . 'config' . DIRECTORY_SEPARATOR);
 define('FOOZ79_DATA_DIR', FOOZ79_ROOT_DIR . 'data' . DIRECTORY_SEPARATOR);
+define('FOOZ79_NS', '\\');
 
 if (!function_exists('getSingleton')) {
     function getSingleton($class, ...$args)
