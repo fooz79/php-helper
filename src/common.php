2dec4940727da3007328d264b42ca160fba12ee8
Alvis Zhang <alvis@1979s.com>
2021-02-27 12:03:52 +0800

    get_caller_class

--- src/common.php
+++ src/common.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 use FooZ79\Statics\Env;
 
 /**
- * This file is part of FooZ79.
+ * helper function from fooz79.
  *
- * @link     https://gitee.com/FooZ/traits
- * @license  https://gitee.com/FooZ/traits/blob/master/LICENSE
+ * @link     https://github.com/fooz79/php-helper
+ * @license  https://github.com/fooz79/php-helper/blob/master/LICENSE
  */
 
 if (strstr(__DIR__, 'vendor')) {
@@ -33,5 +33,37 @@ if (!function_exists('env')) {
     function env(string $section, string $setting, $default = null)
     {
         return Env::get($section . '.' . $setting,  $default);
+    }
+}
+
+if (!function_exists('get_caller_class')) {
+    /**
+     * Get the name of the external object calling the current object
+     *
+     * @param boolean $shortName    use short names
+     * @return string|null
+     */
+    function get_caller_class(bool $shortName = false): ?string
+    {
+        $trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+        $count = count($trace);
+        if ($count > 2) {
+            $file = $trace[0]['file'];
+            $class = $trace[1]['class'];
+            for ($i = 1; $i <= $count; ++$i) {
+                if ($file != $trace[$i + 1]['file']) {
+                    if ($class != $trace[$i]['class']) {
+                        if ($shortName) {
+                            return (new \ReflectionClass($trace[$i]['class']))->getShortName();;
+                        } else {
+                            return $trace[$i]['class'];
+                        }
+                    } else {
+                        $class = $trace[$i]['class'];
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
